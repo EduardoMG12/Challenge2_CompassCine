c242a7a3bda2690cf04a263c28e9116fa79c2fa2
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2024-04-08 10:20:27 -0300

    fix: remove configs on router and add on server/config

--- config/server.ts
+++ config/server.ts
@@ -2,11 +2,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { join } from 'path';
 import * as dotenv from 'dotenv';
-
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.static(join(__dirname, '/../src/public')));
+
+app.use(express.json());
 
 app.use(bodyParser.urlencoded({extended: true}));
 
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import app from '../config/server';
+import { router } from './router';
+
+app.use('/register',router);
