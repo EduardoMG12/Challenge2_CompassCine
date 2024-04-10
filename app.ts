447cb9ed8d097293bb06953ec09699973766c11a
Fábio Silva <106494125+fabusilva@users.noreply.github.com>
2024-04-10 10:40:23 -0300

    fix: Changing postgres database to mongodb

--- app.ts
+++ app.ts
@@ -1,12 +1,11 @@
 import app  from './config/server';
 
-const PORT = process.env.PORT;
-const dbHost = process.env.DB_HOST;
+const PORT = process.env.API_PORT;
+const dbHost = 'localhost';
 
 app.get('/', (req, res) => {
 	res.send('Hello World, route test project configured!');
 });
-
 
 app.listen(PORT, () => console.log(`
 
