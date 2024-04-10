0490a7374193dec6a79540c75b6718847d61ab60
Eduardo Mello <102185386+EduardoMG12@users.noreply.github.com>
2024-04-10 00:55:18 -0300

    Hotfix session crud (#3)
    * refactor: removing all the things I did unnecessarily because I didn't realize it already existed in the code
    
    * feat:  have added the correct code, the Room repository, and fixed the interface.
    
    ## Description
    Pull Request: Correção de Lógica e Refatoração
    Descrição:
    
    Neste pull request, corrigi a lógica da implementação da sessão, pois inicialmente não percebi que a lógica já existia no código.
    
    Detalhes:
    
    O código da feat-session-CRUD foi completamente removido, pois a lógica já estava presente no código base.
    O repositório foi adicionado no local correto para garantir a correta implementação da lógica de sessão.
    
    Achei um erro proveitoso pois utilizei pela primeira vez o git stash e juntamente com isso aprendi um pouco sobre a arquitetura da nossa aplicacao, que deveras achei muito bem organizada

--- src/Repository/IRoomRepository.ts
+++ src/Repository/IRoomRepository.ts
@@ -3,10 +3,10 @@ import { ICreateRoom } from '../DTO/CreateRoomDTO';
 
 
 export interface IRoomRepository{
-    findByName(nome: string): Promise<Sala>;
+    findByName(nome: string): Promise<Sala | null>;
     findByAll(): Promise<Sala[]>
     save(sala: ICreateRoom): Promise<Sala>;
-    findById(id: number): Promise<Sala>;
-    delete(id: string): Promise<Sala>;
+    findById(id: number): Promise<Sala | null>;
+    delete(id: number): Promise<Sala>;
     repository(): Promise<PrismaClient>
 }
--- src/modules/CreateSession/index.ts
+++
@@ -1,7 +0,0 @@
-import { PostgresSessionRepository } from '../../Repository/Implementations/PostgresSessionRepository';
-import { CreateSessionUseCase } from './CreateSessionUseCase';
-import { CreateSessionController } from './CreateSessionController';
-
-export const postgresSessionRepository = new PostgresSessionRepository();
-export const createSessionUseCase = new CreateSessionUseCase(postgresSessionRepository);
-export const createRoomController = new CreateSessionController(createSessionUseCase);
