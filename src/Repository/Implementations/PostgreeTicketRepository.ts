c3c265b7a6342979c4a9e16d11c15cb43eb44f97
Edenilson costa <86031108+gameprime@users.noreply.github.com>
2024-04-12 15:18:16 -0300

    creat functions on variables

--- src/Repository/Implementations/PostgreeTicketRepository.ts
+++ src/Repository/Implementations/PostgreeTicketRepository.ts
@@ -1,22 +1,24 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { ICreateSessionDTO } from '../../DTO/CreateSessionDTO';
 import { ITicketRepository } from '../ITicketRepository';
 import { Ingresso } from '../../models/ingresso';
 import { ICreateTicket } from '../../DTO/CreateTicket';
 
 export class PostgreeTicketRepository implements ITicketRepository{
 	async findById(id: string): Promise<any> {
-		return  null;
-	}
-	findByAll(): Promise<any[]> {
-		throw new Error('Method not implemented.');
+		const ticket = await (await this.repository()).findUnique({where: {id}});
+		return ticket;	}
+	async findByAll(): Promise<any[]> {
+		const tickets = await (await this.repository()).findMany();
+		return tickets;
+
 	}
 	async save(ticket: ICreateTicket): Promise<any> {
 		const objectMovie =  new (await this.repository())(ticket);
 		return await objectMovie.save();
 	}
-	delete(id: string): Promise<any> {
-		throw new Error('Method not implemented.');
+	async delete(id: string): Promise<any> {
+		const ticket = await (await this.repository()).delete({where:{id}});
+		return ticket; 
 	}
 	async repository(): Promise<any> {
 		return Ingresso;
