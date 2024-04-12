bfd01d6d9eec91b6b8fa041fe566c751e2bc751e
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2024-04-12 14:41:43 -0300

    refactor: to adapt the session repository to mongoose

--- src/Repository/Implementations/PostgresSessionRepository.ts
+++ src/Repository/Implementations/PostgresSessionRepository.ts
@@ -2,26 +2,25 @@
 
 import { ICreateSessionDTO } from '../../DTO/CreateSessionDTO';
 import { ISessionRepository } from './../ISessionRepository';
-import { sala }
-
+import { Sessao } from '../../models/sessao';
 
 export class PostgresSessionRepository implements ISessionRepository{
 	async save(session: ICreateSessionDTO): Promise<any> {
 		
 	} // I Don't Know implement this controller
 	async findByAll(): Promise<{ id: string; filmeId: string; salaId: string; horario: Date; }[]> {
-		const sessions = await (await this.repository()).sessao.findMany();
+		const sessions = await (await this.repository()).findMany();
 		return sessions;
 	}
 	async findById(id: string): Promise<{ id: string; filmeId: string; salaId: string; horario: Date; } | null> {
-		const session = await (await this.repository()).sessao.findUnique({where: {id}});
+		const session = await (await this.repository()).findUnique({where: {id}});
 		return session;
 	}
 	async delete(id: string): Promise<{ id: string; filmeId: string; salaId: string; horario: Date; }> {
-		const session = await (await this.repository()).sessao.delete({where:{id}});
+		const session = await (await this.repository()).delete({where:{id}});
 		return session; 
 	}
 	async repository(): Promise<any> {
-		return await prismaClient;
+		return Sessao;
 	}
 }
