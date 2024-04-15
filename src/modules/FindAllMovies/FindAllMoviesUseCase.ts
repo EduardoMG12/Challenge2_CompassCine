90afc4d5b432971771226121d455421492ebe03d
PatrickSampa <sampa.mendonca@gmail.com>
2024-04-14 21:44:58 -0300

    feat: inserindo a busca das sessoes nas propriedades de filme

--- src/modules/FindAllMovies/FindAllMoviesUseCase.ts
+++ src/modules/FindAllMovies/FindAllMoviesUseCase.ts
@@ -1,11 +1,27 @@
 import { IMovieRepository } from '../../Repository/IMovieRepository';
+import { ISessionRepository } from '../../Repository/ISessionRepository';
 
 export class findAllMovieUseCase{
 	private movieRepository: IMovieRepository;
-	constructor(movieRepository: IMovieRepository){
+	private sessaoReposytory: ISessionRepository;
+	constructor(movieRepository: IMovieRepository, sessaoRepository: ISessionRepository){
 		this.movieRepository = movieRepository;
+		this.sessaoReposytory = sessaoRepository;
 	}
 	async execute(){
-		return this.movieRepository.findByAll();
+		const movies =  await this.movieRepository.findByAll();
+		const newArrayObject = [];
+		for (let i = 0;  i < movies.length; i++){
+			for (let h = 0; h < movies[i].sessoes.length; h++){
+				const sessionId = (movies[i].sessoes[h].toString());
+				const session = (await this.sessaoReposytory.findById(sessionId));
+				newArrayObject.push(session);
+			}
+			movies[i].sessoes = newArrayObject;
+			
+		}
+		
+
+		return movies;
 	}
 }
--- src/modules/FindAllMovies/index.ts
+++ src/modules/FindAllMovies/index.ts
@@ -1,7 +1,10 @@
+
 import { MovieRepository } from '../../Repository/Implementations/MovieRepository';
+import { SessionRepository } from '../../Repository/Implementations/SessionRepository';
 import { findAllMovieController } from './FindAllMoviesController';
 import { findAllMovieUseCase } from './FindAllMoviesUseCase';
 
+export const sessaoRepository = new SessionRepository();
 export const movieRepository = new MovieRepository();
-export const FindAllMoviesUsercase = new findAllMovieUseCase(movieRepository);
+export const FindAllMoviesUsercase = new findAllMovieUseCase(movieRepository,sessaoRepository);
 export const FindAllMoviesController = new findAllMovieController(FindAllMoviesUsercase);
